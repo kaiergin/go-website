460430369d3df423cdea8fb8593849fc240b4ad3
kai <kergin2016@gmail.com>
2016-08-14 19:44:36 -0400

    Panic push

--- submit.php
+++ submit.php
@@ -1,23 +1,42 @@
 <?php
-canUpload = 1;
-$target_dir = $_POST["ainame"]."/";
+
+# Make else statements
+
+$canUpload = 1;
+$target_dir = "/home/ec2-user/NewAIs/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$name = $_POST["ainame"];
+$pass = $_POST["aipass"];
 if ($fileType != "go") {
-	canUpload = 0;
+	$canUpload = 0;
+	echo "not go file";
 }
 if ($_POST["ainame"] != basename($_FILES["fileToUpload"]["name"]),".go") {
-	canUpload = 0;
+	$canUpload = 0;
+	echo "not go file type";
 }
 if ($_FILES["fileToUpload"]["size"] > 1000000) {
 	canUpload = 0;
+	echo "too big";
 }
-if (canUpload == 1) {
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-	system("./evaluate")
-} else {
-    echo "Sorry, there was an error uploading your file.";
+$xml=simplexml_load_file("/home/ec2-user/users.xml") or die("Error: Cannot create object");
+$isThere = False;
+for ($i=0,$i<count($xml->player),$i++) {
+	if ($xml->player[$i]->username == $name && $xml->player[$i]->password == $pass) {
+		$isThere = True;
+	}
 }
+if (! $isThere) {
+	$canUpload = 0
+	echo "not a user";
+}
+if ($canUpload == 1) {
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    	echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		#system("./evaluate")
+	} else {
+ 	   echo "Sorry, there was an error uploading your file.";
+	}
 }
 ?>
